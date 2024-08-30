6bb9d4c8ea1096eb568545fedec108edf69af1b8
Michael Abon <via-github@michaelabon.ca>
2024-08-29 22:33:23 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const today = new Date();
+
+function isSameDate(today, candidate) {
+    return (
+        today.getFullYear() === candidate.getFullYear() &&
+        today.getMonth() === candidate.getMonth() &&
+        today.getDate() === candidate.getDate()
+    );
+}
+
+
+const fireworksDates = [
+    new Date('2024-08-30T12:00:00-06:00'),
+]
+
+announceFireworks = () => {
+    document.getElementById('answer').innerHTML = "<p>Yes, Waterfront Drive is <strong><em>closed</em></strong> today.</p> <p>Your evening commute will be disrupted.</p>";
+    document.querySelector('body').classList.add('yes');
+}
+
+// Compare today to the candidate
+for (let fireworksDate of fireworksDates) {
+    if (isSameDate(today, fireworksDate)) {
+        announceFireworks()
+        break;
+    }
+}
